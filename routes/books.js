5d71fdd57d6528e23bec199f7a44658f1d51fe2a
Mahad56 <mosma003@campus.goldsmiths.ac.uk>
2025-11-11 02:13:16 +0000

    Initial commit

---
+++ routes/books.js
@@ -0,0 +1,23 @@
+// Create a new router
+const express = require("express")
+const router = express.Router()
+
+router.get('/search',function(req, res, next){
+    res.render("search.ejs")
+});
+
+router.get('/search-result', function (req, res, next) {
+    //searching in the database
+    res.send("You searched for: " + req.query.keyword)
+});
+
+router.get('/list', function(req, res, next) {
+  const sqlquery = "SELECT * FROM books";
+  db.query(sqlquery, (err, result) => {
+    if (err) return next(err);
+    res.send(result);
+  });
+});
+
+// Export the router object so index.js can access it
+module.exports = router
