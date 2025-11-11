c8c675786214047d1f1edbb475388e962717afd2
Mahad56 <mosma003@campus.goldsmiths.ac.uk>
2025-11-11 14:13:43 +0000

    Completed all ejs files

--- routes/books.js
+++ routes/books.js
@@ -2,22 +2,47 @@
 const express = require("express")
 const router = express.Router()
 
-router.get('/search',function(req, res, next){
-    res.render("search.ejs")
-});
-
-router.get('/search-result', function (req, res, next) {
-    //searching in the database
-    res.send("You searched for: " + req.query.keyword)
-});
-
+//  List all books (EJS table instead of JSON)
 router.get('/list', function(req, res, next) {
   const sqlquery = "SELECT * FROM books";
   db.query(sqlquery, (err, result) => {
     if (err) return next(err);
-    res.send(result);
+    res.render("books/list", { books: result });  // pass data to view
   });
 });
 
-// Export the router object so index.js can access it
-module.exports = router
+// Search form
+router.get('/search', function(req, res) {
+  res.render("books/search");
+});
+
+//  Search database
+router.get(['/search-result', '/search_result'], function (req, res, next) {
+  const term = (req.query.keyword || req.query.search_text || '').trim();
+  const sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+  
+  db.query(sqlquery, [`%${term}%`], (err, result) => {
+    if (err) return next(err);
+    res.render("books/search_results", { books: result, term });
+  });
+});
+
+//  Add Book form
+router.get('/add', function(req, res) {
+  res.render('books/add');
+});
+
+// Add Book submit
+router.post('/add', function (req, res, next) {
+  const name = req.body.name;
+  const price = req.body.price;
+
+  const sqlquery = "INSERT INTO books (name, price) VALUES (?, ?)";
+  db.query(sqlquery, [name, price], (err, result) => {
+    if (err) return next(err);
+    res.redirect('/books/list');
+  });
+});
+
+//  Export router
+module.exports = router;
