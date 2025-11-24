9e27afe5800fdde7f83a971f72886ebd8dfd10fb
Mahad56 <mosma003@campus.goldsmiths.ac.uk>
2025-11-24 22:39:47 +0000

    Log out added

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,15 @@
 const express = require("express")
 const router = express.Router()
 
+// Protect routes
+const redirectLogin = (req, res, next) => {
+    if (!req.session.userId) {
+        res.redirect('/users/login')
+    } else {
+        next()
+    }
+}
+
 // Home page
 router.get('/', function (req, res) {
     res.render('index', { shopData: req.app.locals.shopData })
@@ -10,6 +19,16 @@ router.get('/', function (req, res) {
 // About page
 router.get('/about', function (req, res) {
     res.render('about', { shopData: req.app.locals.shopData })
+})
+
+// Logout (Lab 8 requirement)
+router.get('/logout', redirectLogin, (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.redirect('./')
+        }
+        res.send('you are now logged out. <a href="./">Home</a>')
+    })
 })
 
 // Export the router
